c9554d33e3b2b9d3ed1e215468ef0aab97a5e2e0
Brian-ONeil <oneilbrianlee@gmail.com>
2023-04-25 16:44:52 -0500

    updating files

--- prepare.py
+++ prepare.py
@@ -12,22 +12,21 @@ from sklearn.model_selection import train_test_split
 
 def prep_telco(telco_df):
     '''
-    This function will clean the the telco dataset
+    This function will clean the the telco dataset, create dummies for four categories, concat, drop unneeded files, then strip to lower case.
     '''
     
-    dummy_df = pd.get_dummies(telco_df[['multiple_lines',
-                                 'online_security',
-                                 'online_backup',
-                                 'device_protection', 
-                                 'tech_support',
-                                 'streaming_tv',
-                                 'streaming_movies', 
+    dummy_df = pd.get_dummies(telco_df[['tech_support',
                                  'contract_type', 
                                  'internet_service_type',
                                  'payment_type']],
                               drop_first=True)
 
     telco_df = pd.concat([telco_df, dummy_df], axis=1)
+    telco_df = telco_df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'gender', 'senior_citizen',
+                                      'partner', 'dependents', 'tenure', 'phone_service', 'multiple_lines', 'online_security',
+                                      'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies',
+                                      'paperless_billing', 'total_charges', 'contract_type', 'internet_service_type', 'payment_type'])
+    telco_df = telco_df.rename(columns=str.lower)
     return telco_df
 
 def split_data(df, stratify_col):
